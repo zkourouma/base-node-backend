6d752c4d452300be97b43cbf2f77a89629d0d5d5
zack kourouma <zack@kourouma.me>
2020-06-02 16:48:08 -0700

    Add pnpm support

--- src/api/users.ts
+++ src/api/users.ts
@@ -1,3 +1,4 @@
+import * as Koa from 'koa';
 import * as Router from '@koa/router';
 
 import * as conn from '../storage';
@@ -5,13 +6,13 @@ import { CandidateUser, NewUser } from '../schema';
 
 const router = new Router({ prefix: '/users' });
 
-router.get('users', '/', async (ctx, next) => {
+router.get('users', '/', async (ctx: Koa.Context, next: Koa.Next) => {
   ctx.body = await conn.listUsers();
 
   await next();
 });
 
-router.post('createUser', '/', async (ctx, next) => {
+router.post('createUser', '/', async (ctx: Koa.Context, next: Koa.Next) => {
   const payload = ctx.request.body as NewUser;
   ctx.body = await conn.createUser(payload).catch((_err) => 'That user already exists');
   ctx.status = 400;
@@ -19,7 +20,7 @@ router.post('createUser', '/', async (ctx, next) => {
   await next();
 });
 
-router.put('editUser', '/:id', async (ctx, next) => {
+router.put('editUser', '/:id', async (ctx: Koa.Context, next: Koa.Next) => {
   const payload = ctx.request.body as CandidateUser;
   const { id } = ctx.params as { id: number };
   ctx.body = await conn.updateUser(id, payload);
@@ -27,7 +28,7 @@ router.put('editUser', '/:id', async (ctx, next) => {
   await next();
 });
 
-router.delete('deleteUser', '/:id', async (ctx, next) => {
+router.delete('deleteUser', '/:id', async (ctx: Koa.Context, next: Koa.Next) => {
   const { id } = ctx.params as { id: number };
   ctx.body = await conn.deleteUser(id);
 
